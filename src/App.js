90bc45ed72ac8e1c41860cc638c41a96965eb2fe
Ahmad Zahid <73899902+ahmadZahid99@users.noreply.github.com>
2022-07-06 02:54:49 -0700

    project sample on tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import FeatureSection from './components/featureSection/FeatureSection';
+import HeroSection from './components/heroSection/HeroSection';
+import Navbar from './components/navbar/Navbar';
+import Testimonials from './components/testimonials/Testimonials';
+import CtaSection from './components/ctaSection/CtaSection';
+import Footer from './components/footer/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <HeroSection />
+      <FeatureSection />
+      <Testimonials />
+      <CtaSection />
+      <Footer />
+    </>
   );
 }
 
